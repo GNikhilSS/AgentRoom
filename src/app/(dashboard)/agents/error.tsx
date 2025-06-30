f9423aefa98cc8d3c081e7f8f5b31940a0d92dac
GNikhilSS <nikhilchowdarygarapati@gmail.com>
2025-07-01 00:18:41 +0530

    09: agents setup

---
+++ src/app/(dashboard)/agents/error.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { ErrorState } from "@/components/error-state";
+
+const ErrorPage = () => {
+    return (
+        <ErrorState
+          title="Error Loading Agents"
+          description="Try Again Later"
+        />
+    );
+};
+
+export default ErrorPage;
