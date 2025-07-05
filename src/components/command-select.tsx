5e56e7bbe992a223c93e17b4c55f7906023e36ae
GNikhilSS <nikhilchowdarygarapati@gmail.com>
2025-07-05 15:03:56 +0530

    17: meetings form

---
+++ src/components/command-select.tsx
@@ -0,0 +1,82 @@
+import { useState } from "react";
+import { Button } from "./ui/button";
+import { ChevronsUpDownIcon } from "lucide-react";
+import { cn } from "@/lib/utils";
+import { 
+    CommandEmpty, 
+    CommandInput, 
+    CommandList, 
+    CommandItem,
+    CommandResponsiveDialog } from "./ui/command";
+
+
+interface Props {
+    options: Array<{
+        id: string;
+        value: string;
+        children: React.ReactNode;
+    }>;
+    onSelect: (value: string) => void;
+    onSearch: (value: string) => void;
+    value: string;
+    placeholder?: string;
+    className?: string;
+    isSearchable?: boolean;
+}
+
+export const CommandSelect = ({
+    options,
+    onSelect,
+    onSearch,
+    value,
+    placeholder = "Select an option",
+    className,
+    isSearchable = true,
+}: Props) => {
+    const [open, setOpen] = useState(false);
+    const selectedOption = options.find(option => option.value === value);
+
+    return (
+        <>
+            <Button
+                onClick={() => setOpen(true)}
+                type="button"
+                variant="outline"
+                className={cn("h-9 justify-between font-normal px-2",
+                    !selectedOption && "text-muted-foreground",
+                    className
+                )}
+            >
+                <div>
+                    {selectedOption?.children ?? placeholder}
+                </div>
+                <ChevronsUpDownIcon />
+            </Button>
+            <CommandResponsiveDialog
+                open={open}
+                onOpenChange={setOpen}
+                shouldFilter={false}
+            >
+                <CommandInput placeholder="Search..." onValueChange={onSearch} />
+                <CommandList>
+                    <CommandEmpty>
+                        <span className="text-muted-foreground text-sm">
+                            No options found.
+                        </span>
+                    </CommandEmpty>
+                        {options.map((option) => (
+                            <CommandItem
+                                key={option.id}
+                                onSelect={() => {
+                                    onSelect(option.value)
+                                    setOpen(false);
+                                }}
+                            >
+                                {option.children}
+                            </CommandItem>
+                        ))}
+                </CommandList>
+            </CommandResponsiveDialog>
+        </>
+    );
+};
